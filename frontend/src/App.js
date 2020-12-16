7960f94b7d2a5f5dfe97038e673f9240abf79340
PRATIK <pratikchaubey05@gmail.com>
2020-12-17 02:08:12 +0530

    7C: 1) Added qty select in productdetails screen 2) Added add to cart handler in productdetails screen. Also updated routes in App.js and created CartScreen.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen";
 
 function App() {
   return (
@@ -15,6 +16,8 @@ function App() {
       <Container>
         <Route path="/" component={HomeScreen} exact />
         <Route path="/product/:id" component={ProductScreen} />
+        {/* Here in Location id? question mark makes ID optional */}
+        <Route path="/cart/:id?" component={CartScreen} />
       </Container>
      </main>
      <Footer />
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,13 +1,14 @@
-import React , { useEffect} from "react";
+import React , { useState, useEffect} from "react";
 import {Link} from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux" ;
-import {Row, Col, Image, ListGroup, Card, Button} from "react-bootstrap";
+import {Row, Col, Image, ListGroup, Card, Button, Form} from "react-bootstrap";
 import Rating from "../components/Rating";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
 import {listProductDetails} from "../actions/productActions";
 
-function ProductScreen({match}){
+function ProductScreen({match, history}){
+    const [qty, setQty] = useState(0);
     const dispatch = useDispatch();
     const productDetails = useSelector(state => state.productDetails);
     const {loading, error, product} = productDetails ;
@@ -15,6 +16,13 @@ function ProductScreen({match}){
     useEffect(()=>{
         dispatch(listProductDetails(match.params.id));
     },[dispatch, match]);
+
+    // Handler to deal with add to cart submit button
+    const addToCartHandler = () =>{
+        // history.push is used to redirect and add the location in history object
+        history.push(`/cart/${match.params.id}?qty=${qty}`)
+    }
+
 
     return(
      <>
@@ -68,8 +76,34 @@ function ProductScreen({match}){
                             </Row>
                         </ListGroup.Item>
                         <hr />
-                        <ListGroup.Item className="listgroupitem pt -0">
-                            <Button className="btn btn-block btn-dark" type="button" disabled={product.countInStock===0}>
+                {/* We are using useState hooks to handle qty */}
+                        {
+                            product.countInStock > 0 && (
+                                <ListGroup.Item className="listgroupitem pb-0">
+                                   <Row> 
+                                   <Col>Qty:</Col>
+                                    <Col>
+                                        <Form.Control as="select" value={qty} onChange={(e)=>setQty(e.target.value)}>
+                                            {
+                                                [...Array(product.countInStock).keys()].map((x) => { return (
+                                                    <option value={x+1} key={x+1}>
+                                                        {x+1}
+                                                    </option>)} )
+                                            }
+                                        </Form.Control>
+                                    </Col>
+                                    </Row>
+
+                                </ListGroup.Item>
+                            )
+                        }
+                        <hr />
+                        <ListGroup.Item className="listgroupitem pt-2">
+                            <Button 
+                            onClick={addToCartHandler}
+                            className="btn btn-block btn-dark" 
+                            type="button" 
+                            disabled={product.countInStock===0}>
                                 Add to Cart
                             </Button>
                         </ListGroup.Item>
