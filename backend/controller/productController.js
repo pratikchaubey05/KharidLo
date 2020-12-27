af8f5999253270bedb70592e11c79725b70a3236
PRATIK <pratikchaubey05@gmail.com>
2020-12-28 02:56:05 +0530

    8C: 1) Performed cleanup: In Backend, Separated functionality to controller from routes.

---
+++ backend/controller/productController.js
@@ -0,0 +1,32 @@
+// Main desc: Dedicated controllers to handle the functionality. This has been done to make code cleaner
+
+// desc: As mongoose returns promise and to Handle exception/error we want to avoid writting try and catch. hence this.
+import asynchandler from "express-async-handler" ;
+import Product from "../models/productModel.js" ;
+
+
+// @desc: Fetch all products
+// @route: GET /api/products
+// @access: Public
+const getProducts = asynchandler(async (req, res) => {
+    const products = await Product.find({}) ;
+    // throw new Error("Testing Error");
+    res.json(products) ;
+})
+
+
+// @desc: Fetch single product
+// @route: GET /api/products/:id
+// @access: Public
+const getProductById = asynchandler(async (req, res) => {
+    const product = await Product.findById(req.params.id) ;
+    if(product){
+    res.json(product) ;
+    }else{
+        res.status(404);
+        throw new Error("Product Not Found!!!"); // As we are using custom error handler i.e., server.js->middlerware/errorMiddleware.js
+        // res.status(404).json({message: "Product not found"}) ;
+    }
+})
+
+export {getProducts, getProductById} ;
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,35 +1,14 @@
 // This is a middleware to handle all the request on "/api/products" 
 // we are using Router(), to handle the requests
+// We will redirect request to respective controller.
 
 import express from "express";
 const router = express.Router();
-// desc: As mongoose returns promise and to Handle exception/error we want to avoid writting try and catch. hence this.
-import asynchandler from "express-async-handler" ;
+import {getProductById, getProducts} from "../controller/productController.js";
 
-import Product from "../models/productModel.js" ;
-
-// @desc: Fetch all products
-// @route: GET /api/products
-// @access: Public
-router.get("/", asynchandler(async(req, res)=>{
-    const products = await Product.find({}) ;
-    // throw new Error("Testing Error");
-    res.json(products) ;
-}));
-
-// @desc: Fetch single product
-// @route: GET /api/products/:id
-// @access: Public
-router.get("/:id", asynchandler(async(req, res)=>{ 
-    const product = await Product.findById(req.params.id) ;
-    if(product){
-    res.json(product) ;
-    }else{
-        res.status(404);
-        throw new Error("Product Not Found!!!"); // As we are using custom error handler i.e., server.js->middlerware/errorMiddleware.js
-        // res.status(404).json({message: "Product not found"}) ;
-    }
-})) ;
+// router.get("/", getProducts);  : We can do like this also but we will use Route() for chaining.
+router.route("/").get(getProducts) ;
+router.route("/:id").get(getProductById) ;
 
 export default router ;
 
